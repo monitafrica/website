7c7f82f9858868bdfd63adef36826ab9fb751f5c
georgeprosperr <georgeprosperr@gmail.com>
2019-06-26 17:01:33 +0300

    fix merge error

--- src/app/product/group_saving/group_saving.component.ts
+++ src/app/product/group_saving/group_saving.component.ts
@@ -12,26 +12,6 @@ export class GroupSavingComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.displaySlider();
-  }
-
-
-  displaySlider() {
-    $(".coverflow-slider").executeFunction("flipster", function () {
-      $(".coverflow-slider").each(function () {
-        if ($(this).attr("data-trigger") != "manual") {
-          var w = $(this).attr("data-width");
-          var wm = $(this).attr("data-mobile-width");
-          if ($(window).width() < 768 && !isEmpty(wm)) w = wm;
-          var options = {};
-          var opt = $(this).attr("data-options");
-          if (!isEmpty(opt)) {
-            options = getOptionsString(opt, options);
-          }
-          if (!isEmpty(w)) $(this).find("ul > li").css("width", w + "%");
-          $(this).flipster(options);
-        }
-      });
-    });
+    $.getScript('assets//HTWF/scripts/script.js');
   }
 }
