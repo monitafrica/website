27b23b35ac599d6890e710ce6a055ae2c83bc95c
georgeprosperr <georgeprosperr@gmail.com>
2019-06-26 12:53:11 +0300

    fix navigate to top and vicoba flexible slider image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
-import {Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'website';
   currentUrl = '';
 
-  constructor(private router: Router) {
-    router.events.subscribe(() => {
-      this.currentUrl = document.location.pathname;
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      this.router.events.subscribe(() => {
+        this.currentUrl = document.location.pathname;
+      });
+
+      // navigate to top.
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0);
     });
   }
 }
--- src/app/product/group_saving/group_saving.component.ts
+++ src/app/product/group_saving/group_saving.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let $;
+
 @Component({
   selector: 'app-group-saving',
   templateUrl: './group_saving.component.html',
@@ -10,6 +12,26 @@ export class GroupSavingComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.displaySlider();
   }
 
+
+  displaySlider() {
+    $(".coverflow-slider").executeFunction("flipster", function () {
+      $(".coverflow-slider").each(function () {
+        if ($(this).attr("data-trigger") != "manual") {
+          var w = $(this).attr("data-width");
+          var wm = $(this).attr("data-mobile-width");
+          if ($(window).width() < 768 && !isEmpty(wm)) w = wm;
+          var options = {};
+          var opt = $(this).attr("data-options");
+          if (!isEmpty(opt)) {
+            options = getOptionsString(opt, options);
+          }
+          if (!isEmpty(w)) $(this).find("ul > li").css("width", w + "%");
+          $(this).flipster(options);
+        }
+      });
+    });
+  }
 }
